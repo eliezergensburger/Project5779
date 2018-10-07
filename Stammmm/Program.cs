2377edb4b11942305c4299b7a8eaa975fd9bd4a7
eliezergensburger <eliezer.jct@gmail.com>
2018-10-07 18:24:29 +0300

    kuku first time

---
+++ Stammmm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stammmm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hell{0} W{0}rld",'o');
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
